7171c0432e7333be4f5fa1aae02ac4cabe4dd0e5
Misha Bergal <misha.bergal@gmail.com>
2012-02-19 03:59:55 -0600

    Assembly.LoadTextResource

---
+++ src/RussianElectionResultsScraper/src/utils/ReadResource.cs
@@ -0,0 +1,13 @@
+﻿using System.IO;
+using System.Reflection;
+
+namespace RussianElectionResultsScraper
+    {
+    public static class AssemblyEx
+        {
+        public static string LoadTextResource( this Assembly assembly, string resourceName )
+            {
+            return new StreamReader( Assembly.GetExecutingAssembly().GetManifestResourceStream(resourceName) ).ReadToEnd();
+            }
+        }
+    }
