fd9969673f7494b03fc0f2db5297d72bd84840f8
Misha Bergal <misha.bergal@gmail.com>
2012-02-19 03:58:41 -0600

    "init-database" command

---
+++ src/RussianElectionResultsScraper/src/commands/InitDatabaseCommand.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using NHibernate.Tool.hbm2ddl;
+
+namespace RussianElectionResultsScraper.src.commands
+{
+    public class InitDatabaseCommand : BaseConsoleCommand
+        {
+        public InitDatabaseCommand()
+            {
+            this.IsCommand("init-database", "Create/recreate necessary objects in database");
+            }
+
+        public override int Run(string[] args)
+            {
+            var lines = new List<string>();
+
+            var schemaExport = new SchemaExport( this.BuildElectionResultsDatabaseConfiguration() );
+            schemaExport.Create(x => lines.Add(x), true);
+            schemaExport.Execute(true, false, false);
+            return 0;
+            }
+        
+        }
+}
