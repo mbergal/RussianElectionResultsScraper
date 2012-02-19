4aa51909f35f6ac44458368494a1ac744a184d24
Misha Bergal <misha.bergal@gmail.com>
2012-02-19 04:00:21 -0600

    Election.Counters utilities

--- src/RussianElectionResultsScraper.Model/Election.cs
+++ src/RussianElectionResultsScraper.Model/Election.cs
@@ -1,17 +1,30 @@
-﻿using Iesi.Collections.Generic;
+﻿using System.Collections.Generic;
+using System.Drawing;
 using System.Linq;
+using Iesi.Collections.Generic;
 
 namespace RussianElectionResultsScraper.Model
 {
     public class Election
         {
+        public Election()
+            {
+            this.Counters = new HashedSet<Model.CounterDescription>();
+            }
         public virtual string                   Id { get; set; }
         public virtual string                   Name { get; set; }
         public virtual VotingPlace              Root { get; set; }
-        public virtual ISet<CounterDescription> Counters { get; set; }
+        public virtual Iesi.Collections.Generic.ISet<CounterDescription> Counters { get; set; }
         public virtual CounterDescription       Counter(int counter)
             {
             return this.Counters.FirstOrDefault(x => x.Counter == counter );
+            }
+        public virtual IList<CounterDescription>       Candidates
+            {
+            get
+                {
+                return Counters.Where( x => x.Counter >= 19 ).ToList();
+                }
             }
         }
 
