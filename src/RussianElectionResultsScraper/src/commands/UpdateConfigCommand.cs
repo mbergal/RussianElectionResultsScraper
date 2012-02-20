2987a3e94c56632bb327c4444545335d9b8e7dd8
Misha Bergal <misha.bergal@gmail.com>
2012-02-19 21:19:49 -0600

    update-config command

---
+++ src/RussianElectionResultsScraper/src/commands/UpdateConfigCommand.cs
@@ -0,0 +1,19 @@
+﻿namespace RussianElectionResultsScraper
+{
+    public class UpdateConfigCommand : BaseConsoleCommand
+        {
+        public UpdateConfigCommand()
+            {
+            this.IsCommand( "update-config", "Update Configuration" );
+            this.HasConfigOption();
+            }
+
+        public override int Run(string[] args)
+            {
+            base.Run(args);
+            SaveElection(LoadConfiguration());
+            return 0;
+            }
+
+        }
+}
