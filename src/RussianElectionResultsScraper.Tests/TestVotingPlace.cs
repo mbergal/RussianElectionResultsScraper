871020661a545f9f26c150a50e62f6c5adf49fcc
Misha Bergal <misha.bergal@gmail.com>
2012-03-01 05:01:19 -0600

    .

--- src/RussianElectionResultsScraper.Tests/TestVotingPlace.cs
+++ src/RussianElectionResultsScraper.Tests/TestVotingPlace.cs
@@ -10,10 +10,10 @@ namespace TestElectionResultsScraper
         public void ShouldBeAbleToSetCounters()
             {
             var vp = new VotingPlace();
-            vp.SetCounter(1, 1);
-            Assert.AreEqual( 1, vp.Counter( 1 ) );
-            vp.SetCounter(1, 1000);
-            Assert.AreEqual(1000, vp.Counter(1));
+            vp.SetCounter( "1", 1);
+            Assert.AreEqual( 1, vp.Counter( "1" ) );
+            vp.SetCounter( "1", 1000);
+            Assert.AreEqual(1000, vp.Counter("1"));
             }
 
         [Test]
@@ -22,19 +22,19 @@ namespace TestElectionResultsScraper
             var election = new Election();
             election.Counters.AddAll( new[]
                                           {
-                                          new CounterDescription() { Counter = 1, Name = "Counter 1" },
-                                          new CounterDescription() { Counter = 2, Name = "Counter 2" },
+                                          new CounterDescription() { Counter = "1", Name = "Counter 1" },
+                                          new CounterDescription() { Counter = "2", Name = "Counter 2" },
                                           } );
 
             var vp = new VotingPlace() { Election = election };
             vp.Children.AddAll( new []
                                     {
-                                    new VotingPlace().SetCounter( 1, 1 ).SetCounter( 2, 2 ),
-                                    new VotingPlace().SetCounter( 1, 100 ).SetCounter( 2, 200 )
+                                    new VotingPlace().SetCounter( "1", 1 ).SetCounter( "2", 2 ),
+                                    new VotingPlace().SetCounter( "1", 100 ).SetCounter( "2", 200 )
                                     } );
             vp.UpdateCountersFromChildren();
-            Assert.AreEqual( 101, vp.Counter( 1 ) );
-            Assert.AreEqual(202, vp.Counter(2));
+            Assert.AreEqual( 101, vp.Counter( "1" ) );
+            Assert.AreEqual(202, vp.Counter( "2") );
             }
         }
     }
