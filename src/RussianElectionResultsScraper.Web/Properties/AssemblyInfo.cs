dba921a95a4c46ec4fe8b8b1bdfe81bad7ba920d
Misha Bergal <misha.bergal@gmail.com>
2012-03-04 03:50:55 -0600

    Removed AssemblyVersion and AssemblyFileVersion since they are autogenerated now

--- src/RussianElectionResultsScraper.Web/Properties/AssemblyInfo.cs
+++ src/RussianElectionResultsScraper.Web/Properties/AssemblyInfo.cs
@@ -21,15 +21,3 @@ using System.Runtime.InteropServices;
 
 // The following GUID is for the ID of the typelib if this project is exposed to COM
 [assembly: Guid("77d3ad87-3da6-4027-aa0d-8a20c75ef423")]
-
-// Version information for an assembly consists of the following four values:
-//
-//      Major Version
-//      Minor Version 
-//      Build Number
-//      Revision
-//
-// You can specify all the values or you can default the Revision and Build Numbers 
-// by using the '*' as shown below:
-[assembly: AssemblyVersion("1.0.0.0")]
-[assembly: AssemblyFileVersion("1.0.0.0")]
