d1acaccf50e872c44eb2edb624c4d95cb1554dae
Misha Bergal <misha.bergal@gmail.com>
2012-03-19 01:21:15 -0500

    NullPageCache

---
+++ src/RussianElectionResultsScraper/src/utils/NullPageCache.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+
+namespace RussianElectionResultsScraper.src.utils
+{
+    public class NullPageCache : IPageCache
+        {
+        public Stream Get(string pageUri)
+            {
+            return null;
+            }
+
+        public void Put(string pageUri, Stream content)
+            {
+            }
+
+        public void Remove(string pageUri)
+            {
+            }
+        }
+}
