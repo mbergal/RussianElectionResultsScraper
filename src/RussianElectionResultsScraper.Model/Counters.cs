217b9d579235f84d7b7759f701509439c63943c6
Misha Bergal <misha.bergal@gmail.com>
2012-03-22 15:56:56 -0500

    Counter aliases

---
+++ src/RussianElectionResultsScraper.Model/Counters.cs
@@ -0,0 +1,20 @@
+﻿namespace RussianElectionResultsScraper.Model
+{
+    public class Counters
+    {
+        public const string А = "а";
+        public const string Б = "б";
+        public const string В = "в";
+        public const string Г = "г";
+        public const string Д = "д";
+        public const string Е = "е";
+        public const string Ж = "ж";
+        public const string З = "з";
+        public const string И = "и";
+        public const string К = "к";
+        public const string Л = "л";
+        public const string М = "м";
+
+        public static readonly string[] Hierarchical = new[] { А, Б, В, Г, Д, Е, Ж, З, И, К, Л, М };
+    };
+}
