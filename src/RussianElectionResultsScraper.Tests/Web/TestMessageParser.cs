86dbe30e1db1b444a1433cb08b4d30c1b489f937
Misha Bergal <misha.bergal@gmail.com>
2012-03-24 03:12:53 -0500

    MessageParser + test

---
+++ src/RussianElectionResultsScraper.Tests/Web/TestMessageParser.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using NUnit.Framework;
+using RussianElectionResultScraper.Web.Infrastructure;
+using RussianElectionResultsScraper.Model;
+
+namespace TestElectionResultsScraper
+    {
+    [TestFixture]
+    public class TestMessageParser
+        {
+        [Test] 
+        public void Should_correctly_parse_messages_without_arguments()
+            {
+            var mp = new MessageParser( s1 => { throw new NotImplementedException(); } );
+            var s = mp.ParseMessage( "aaaa" );
+            Assert.AreEqual( "aaaa", s.Format );
+            Assert.AreEqual( 0, s.Args.Length );
+            }
+        [Test] 
+        public void Should_correctly_parse_messages_with_one_argument()
+            {
+            var mp = new MessageParser( s1 => new VotingPlace() { Name = "Name 123"} );
+            var s = mp.ParseMessage( "aaaa {vp:123}" );
+            Assert.AreEqual( "aaaa {0}", s.Format );
+            Assert.AreEqual( 1, s.Args.Length );
+            Assert.IsInstanceOf( typeof( VotingPlace ), s.Args[0] );
+            Assert.AreEqual( "Name 123", ((VotingPlace)s.Args[0]).Name );
+            }
+        [Test] 
+        public void Should_correctly_parse_messages_with_multiple_argument()
+            {
+            var mp = new MessageParser( s1 => new VotingPlace() {Name = "Name " + s1 } );
+            var s = mp.ParseMessage( "aaaa {vp:123} bbbb {vp:456}" );
+            Assert.AreEqual( "aaaa {0} bbbb {1}", s.Format );
+            Assert.AreEqual( 2, s.Args.Length );
+            Assert.IsInstanceOf( typeof( VotingPlace ), s.Args[1] );
+            Assert.AreEqual( "Name 456", ((VotingPlace)s.Args[1]).Name );
+            }
+        }
+
+    }
---
+++ src/RussianElectionResultsScraper.Web/Infrastructure/MessageParser.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text.RegularExpressions;
+using RussianElectionResultsScraper.Model;
+
+namespace RussianElectionResultScraper.Web.Infrastructure
+    {
+    public class MessageParser
+        {
+        static readonly Regex vpRegex = new Regex( @"\{vp:(\d+)\}" );
+        private readonly Func<string,VotingPlace>   _votingPlaceFactory;
+
+        public MessageParser( Func<string,VotingPlace> votingPlaceFactory )
+            {
+            this._votingPlaceFactory = votingPlaceFactory;
+            }
+
+
+        public ParameterizedString ParseMessage( string message )
+            {
+            if (message != null)
+                {
+                int argPos = 0;
+                var args = new List<object>();
+                var format = vpRegex.Replace( message,
+                                              match =>
+                                                  {
+                                                  var votingPlace = this._votingPlaceFactory( match.Groups[1].Value );
+                                                  args.Add( votingPlace );
+                                                  return string.Format( "{{{0}}}", argPos++ );
+                                                  }
+                                               );
+                return new ParameterizedString( format, args.ToArray() );
+                }
+            else
+                {
+                return new ParameterizedString( "" );
+                }
+            }
+
+        }
+
+}
