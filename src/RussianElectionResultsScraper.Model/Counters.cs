1c522df50ef532c487a5b795d88988340e191fab
Misha Bergal <misha.bergal@gmail.com>
2012-03-24 02:55:49 -0500

    Counters match counter names on CIK site

--- src/RussianElectionResultsScraper.Model/Counters.cs
+++ src/RussianElectionResultsScraper.Model/Counters.cs
@@ -1,20 +1,31 @@
-﻿namespace RussianElectionResultsScraper.Model
-{
-    public class Counters
+﻿using System.Diagnostics.CodeAnalysis;
+
+namespace RussianElectionResultsScraper.Model
     {
-        public const string А = "а";
-        public const string Б = "б";
-        public const string В = "в";
-        public const string Г = "г";
-        public const string Д = "д";
-        public const string Е = "е";
-        public const string Ж = "ж";
-        public const string З = "з";
+    [SuppressMessage( "StyleCop.CSharp.NamingRules", "SA1303:ConstFieldNamesMustBeginWithUpperCaseLetter",
+        Justification = "Reviewed. Suppression is OK here." )] 
+    public class Counters
+        {
+        public const string _1 = "1";
+        public const string _2 = "2";
+        public const string _3 = "3";
+        public const string _4 = "4";
+        public const string _5 = "5";
+        public const string _6 = "6";
+        public const string а = "а";
+        public const string б = "б";
+
+        public const string в = "в";
+        public const string г = "г";
+        public const string д = "д";
+        public const string е = "е";
+        public const string ж = "ж";
+        public const string з = "з";
         public const string И = "и";
         public const string К = "к";
         public const string Л = "л";
         public const string М = "м";
 
-        public static readonly string[] Hierarchical = new[] { А, Б, В, Г, Д, Е, Ж, З, И, К, Л, М };
-    };
-}
+        public static readonly string[] Hierarchical = new[] {а, б, в, г, д, е, ж, з, И, К, Л, М};
+        }
+    }
