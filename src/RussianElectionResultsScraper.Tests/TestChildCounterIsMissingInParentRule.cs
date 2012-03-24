fabf2e7df987e7aa016b55e7dd4947ca98c4261e
Misha Bergal <misha.bergal@gmail.com>
2012-03-24 03:24:07 -0500

    Test for ChildCounterIsMissingInParentRule

---
+++ src/RussianElectionResultsScraper.Tests/TestChildCounterIsMissingInParentRule.cs
@@ -0,0 +1,30 @@
+﻿using System.Linq;
+using NUnit.Framework;
+using RussianElectionResultsScraper.Model;
+using RussianElectionResultsScraper.Model.Validation;
+
+namespace TestElectionResultsScraper
+    {
+    [TestFixture]
+    public class TestChildCounterIsMissingInParentRule
+        {
+        [Test]
+        public void Should_identify_problem_when_parent_counter_does_not_exist()
+            {
+            ValidationVotingPlace child1;
+            var parent = Factory.MakeVotingPlace( 
+                type: Type.TIK,
+                children: new []
+                              {
+                              child1 = Factory.MakeVotingPlace( counters: new[] { Factory.MakeVotingResult( Counters._1, 1) }),
+                              Factory.MakeVotingPlace( counters: new[] { Factory.MakeVotingResult( Counters._1, 1) })
+                              });
+
+            var problems = new ChildCounterIsMissingInParentRule( child1 ).Check( child1.Results[ Counters._1 ] ).ToList();
+            Assert.That(problems.Count, Is.EqualTo( 1 ));
+            Assert.That(problems[0], Is.InstanceOf<ChildCounterIsMissingInParent>());
+            }
+
+
+        }
+    }
