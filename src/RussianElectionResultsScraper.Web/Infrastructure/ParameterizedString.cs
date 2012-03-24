4b5d068c4008b700b5cc6d55754fa886e64499d0
Misha Bergal <misha.bergal@gmail.com>
2012-03-24 03:52:52 -0500

    ParameterizedString - format + parametrs

---
+++ src/RussianElectionResultsScraper.Web/Infrastructure/ParameterizedString.cs
@@ -0,0 +1,24 @@
+﻿namespace RussianElectionResultScraper.Web.Infrastructure
+    {
+    public class ParameterizedString
+        {
+        private readonly string      _format;
+        private readonly object[]    _args;
+
+        public ParameterizedString( string format, params object[] args )
+            {
+            this._format = format;
+            this._args = args;
+            }
+
+        public string Format
+            {
+            get { return _format; }
+            }
+
+        public object[] Args
+            {
+            get { return this._args;  }
+            }
+        }
+    }
