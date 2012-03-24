98a2233cf191ff2bc4abfc3e917533c6fc1ba0fa
Misha Bergal <misha.bergal@gmail.com>
2012-03-24 03:52:30 -0500

    Error model and view

---
+++ src/RussianElectionResultsScraper.Web/Models/ErrorModel.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace RussianElectionResultScraper.Web
+    {
+    public class ErrorModel
+        {
+        private readonly Exception _error;
+
+        public ErrorModel( Exception error )
+            {
+            this._error = error;
+            }
+
+        public string Title
+            {
+            get { return this._error.Message; }
+            }
+        }
+
+    }
