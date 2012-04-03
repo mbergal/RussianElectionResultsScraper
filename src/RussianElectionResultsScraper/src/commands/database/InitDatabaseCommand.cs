44350b6ff612d9b3275559b70456b5d0e467ca6f
Misha Bergal <misha.bergal@gmail.com>
2012-04-03 03:36:20 -0500

    InitDatabaseCommand - add provider name

--- src/RussianElectionResultsScraper/src/commands/database/InitDatabaseCommand.cs
+++ src/RussianElectionResultsScraper/src/commands/database/InitDatabaseCommand.cs
@@ -2,14 +2,14 @@
 {
     public class InitDatabaseCommand : BaseCommand
         {
-        public InitDatabaseCommand( string connectionString )
-            : base( connectionString: connectionString )
+        public InitDatabaseCommand( string connectionString, string providerName )
+            : base( connectionString: connectionString, providerName: providerName )
             {
             }
 
         public override int Execute()
             {
-            var runner = new RussianElectionResultsScraper.MigrationRunner.MigrationRunner( this._providerName );
+            var runner = new MigrationRunner.MigrationRunner( this._providerName );
             runner.MigrateUp( this._connectionString );
             return 0;
             }
